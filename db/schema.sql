a9711a72dffe64acd12c21d464b9dccb59af2f5a
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-05-21 20:36:34 -0600

    set up department table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,8 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
+    department_id INT NOT NULL PRIMARY KEY,
+    department_name  VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee (
