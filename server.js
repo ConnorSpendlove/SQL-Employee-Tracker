6e1dab80394fca8414c3e004da6c21f2817f83f3
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-05-22 12:23:21 -0600

    refactored server.js to intake proper id titles that match the schema

--- server.js
+++ server.js
@@ -54,8 +54,8 @@ async function init() {
 // View all departments
 function viewAllDepartments() {
   const query = `
-    SELECT department_id as "Department ID",
-           department_name as "Department Name"
+    SELECT id as "Department ID",
+           name as "Department Name"
     FROM department;`;
   db.query(query, (err, results) => {
     if (err) throw err;
@@ -67,12 +67,12 @@ function viewAllDepartments() {
 // View all roles
 function viewAllRoles() {
   const query = `
-    SELECT role_id as "Role ID",
-           title as "Role Title",
-           salary as "Role Salary",
-           department_name as "Department Name"
+    SELECT role.id as "Role ID",
+           role.title as "Role Title",
+           role.salary as "Role Salary",
+           department.name as "Department Name"
     FROM role
-    LEFT JOIN department ON role.department_id = department.department_id;`;
+    LEFT JOIN department ON role.department_id = department.id;`;
   db.query(query, (err, results) => {
     if (err) throw err;
     console.table(results);
@@ -83,16 +83,16 @@ function viewAllRoles() {
 // View all employees
 function viewAllEmployees() {
   const query = `
-    SELECT employee.employee_id as "Employee ID",
+    SELECT employee.id as "Employee ID",
            CONCAT(employee.first_name, " ", employee.last_name) as "Employee Name",
            CONCAT(IFNULL(manager.first_name, "NO MANAGER ASSIGNED"), " ", IFNULL(manager.last_name, "")) as "Manager",
            role.title as "Role",
-           department.department_name as "Department",
+           department.name as "Department",
            role.salary as "Salary"
     FROM employee
-    LEFT JOIN role ON employee.role_id = role.role_id
-    LEFT JOIN department ON role.department_id = department.department_id
-    LEFT JOIN employee manager ON employee.manager_id = manager.employee_id;`;
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id;`;
   db.query(query, (err, results) => {
     if (err) throw err;
     console.table(results);
@@ -107,11 +107,11 @@ async function addDepartment() {
     {
       type: "input",
       name: "departmentName",
-      message: "Please, enter a department name:",
+      message: "Enter a department name:",
     },
   ]);
   const query = `
-    INSERT INTO department (department_name)
+    INSERT INTO department (name)
     VALUES (?);`;
   db.query(query, [answer.departmentName], (err, results) => {
     if (err) throw err;
@@ -128,25 +128,25 @@ async function addRole() {
       {
         type: "input",
         name: "roleName",
-        message: "Please, enter a role to be added:",
+        message: "Enter a role to be added:",
       },
       {
         type: "input",
         name: "roleSalary",
-        message: "Please, enter the salary for the role:",
+        message: "Enter the salary for the role:",
       },
       {
         type: "list",
         name: "departmentName",
-        message: "Please, select department for this role:",
-        choices: results.map(department => department.department_name),
+        message: "Select department for this role:",
+        choices: results.map(department => department.name),
       },
     ]);
-    const department = results.find(dept => dept.department_name === answer.departmentName);
+    const department = results.find(dept => dept.name === answer.departmentName);
     const query = `
       INSERT INTO role (title, salary, department_id)
       VALUES (?, ?, ?);`;
-    db.query(query, [answer.roleName, answer.roleSalary, department.department_id], (err, results) => {
+    db.query(query, [answer.roleName, answer.roleSalary, department.id], (err, results) => {
       if (err) throw err;
       viewAllRoles();
     });
@@ -156,35 +156,35 @@ async function addRole() {
 // Add an employee
 async function addEmployee() {
   const inquirer = await import('inquirer');
-  db.query("SELECT role_id, title FROM role;", (err, roleResults) => {
+  db.query("SELECT id, title FROM role;", (err, roleResults) => {
     if (err) throw err;
-    const roles = roleResults.map(role => ({ name: role.title, value: role.role_id }));
+    const roles = roleResults.map(role => ({ name: role.title, value: role.id }));
 
-    db.query("SELECT employee_id, CONCAT(first_name, ' ', last_name) as EmployeeName FROM employee;", async (err, employeeResults) => {
+    db.query("SELECT id, CONCAT(first_name, ' ', last_name) as EmployeeName FROM employee;", async (err, employeeResults) => {
       if (err) throw err;
-      const managers = employeeResults.map(manager => ({ name: manager.EmployeeName, value: manager.employee_id }));
+      const managers = employeeResults.map(manager => ({ name: manager.EmployeeName, value: manager.id }));
 
       const answer = await inquirer.default.prompt([
         {
           type: "input",
           name: "firstName",
-          message: "Please, enter employee first name to be added:",
+          message: "Enter employee first name to be added:",
         },
         {
           type: "input",
           name: "lastName",
-          message: "Please, enter employee last name to be added:",
+          message: "Enter employee last name to be added:",
         },
         {
           type: "list",
           name: "roleId",
-          message: "Please, choose a role:",
+          message: "Choose a role:",
           choices: roles,
         },
         {
           type: "list",
           name: "managerId",
-          message: "Please, choose a manager:",
+          message: "Choose a manager:",
           choices: managers,
         },
       ]);
@@ -203,23 +203,23 @@ async function addEmployee() {
 async function updateEmployeeRole() {
   const inquirer = await import('inquirer');
   db.query(`
-    SELECT employee_id, CONCAT(first_name, ' ', last_name) as EmployeeName, role.title
+    SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) as EmployeeName, role.title
     FROM employee
-    LEFT JOIN role ON employee.role_id = role.role_id;`, (err, employeeResults) => {
+    LEFT JOIN role ON employee.role_id = role.id;`, (err, employeeResults) => {
     if (err) throw err;
-    db.query("SELECT role_id, title FROM role;", async (err, roleResults) => {
+    db.query("SELECT id, title FROM role;", async (err, roleResults) => {
       if (err) throw err;
       const answer = await inquirer.default.prompt([
         {
           type: "list",
           name: "employeeName",
-          message: "Please, select the employee you want to update:",
+          message: "Select an employee to update:",
           choices: employeeResults.map(employee => employee.EmployeeName),
         },
         {
           type: "list",
           name: "roleTitle",
-          message: "Please, select the new role you want to assign to the employee:",
+          message: "Select the new role you want to assign to the employee:",
           choices: roleResults.map(role => role.title),
         },
       ]);
@@ -227,8 +227,8 @@ async function updateEmployeeRole() {
       const role = roleResults.find(role => role.title === answer.roleTitle);
       const query = `
         UPDATE employee SET role_id = ?
-        WHERE employee_id = ?;`;
-      db.query(query, [role.role_id, employee.employee_id], (err, results) => {
+        WHERE id = ?;`;
+      db.query(query, [role.id, employee.id], (err, results) => {
         if (err) throw err;
         viewAllEmployees();
       });
@@ -239,7 +239,7 @@ async function updateEmployeeRole() {
 // End process
 function quit() {
   db.end();
-  console.info("I will see you later!");
+  console.info("Bye");
 }
 
 // Call Init function
